becd1b913680980d99d54f0f88018898fed851c3
Vishnu <shendgevishnu308@gmail.com>
2023-08-16 18:01:13 +0530

    19. Created and updated src/textSummarizer/pipeline/stage_02_data_validation.py

--- src/textSummarizer/pipeline/stage_01_data_ingestion.py
+++ src/textSummarizer/pipeline/stage_01_data_ingestion.py
@@ -5,6 +5,7 @@ from textSummarizer.logging import logger
 class DataIngestionTrainingPipeline:
     def __init__(self):
         pass
+    
     def main(self):
         config = ConfigurationManager()
         data_ingestion_config = config.get_data_ingestion_config()
